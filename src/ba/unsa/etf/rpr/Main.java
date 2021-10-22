35fb7e1ec0ca4ca22e9b42921fed3e85adb4c3c3
emaslesa1 <emaslesa1@etf.unsa.ba>
2021-10-22 12:41:59 +0200

    Zadatak 2

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int sumCifara(int n) {
-        int s=0;
-        while(n!=0) {
-            s+=n%10;
+    private static int sumaCifara(int n){
+        int suma=0;
+        while(n!=0){
+            suma+=n%10;
             n/=10;
         }
-        return s;
+        return suma;
     }
+
     public static void main(String[] args) {
-        Scanner ulaz = new Scanner(System.in);
-        int n;
-        n=ulaz.nextInt();
-        for(int i=2; i<n; i++) {
-            if(i%sumCifara(i)==0)
-                System.out.println(i + " ");
+	    Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        int i=2;
+        while(i<n){
+            if(i%sumaCifara(i)==0) System.out.println(i + " ");
+            i++;
         }
-        // write your code here
     }
 }
